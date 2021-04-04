7d37a39cef3e2c4d332ab69f3081a05c6be62228
Benjamin Stehmans <b.stehmans@oraylis.de>
2021-04-04 12:54:22 +0200

    Fix Date issue

--- SQL/Procedures/dbo/set_dayinoffice.sql
+++ SQL/Procedures/dbo/set_dayinoffice.sql
@@ -11,19 +11,17 @@ CREATE PROCEDURE [dbo].[setdayinoffice]
 (
     -- Add the parameters for the stored procedure here
    @employee as nvarchar (100), 
-   @office_date as datetime2(7)
+   @office_date as date
 )
 AS
 BEGIN
   DECLARE @datetime as datetime2(7)= getutcdate();
 
-
-
     Merge [dbo].[dayinoffice] as t
 	Using (Select @office_date,@employee , @datetime) as s (office_date,employee,insert_time) 
 	On (t.[employee] = s.employee and t.[office_date] = s.[office_date])
 	WHEN MATCHED THEN 
-		UPDATE SET [insert_time] = s.[insert_time]
+		UPDATE SET t.[insert_time] = s.[insert_time]
 	WHEN NOT MATCHED THEN
 		INSERT ([office_date] ,[employee]  ,[insert_time])
 		Values (s.office_date, s.employee,s.insert_time);
